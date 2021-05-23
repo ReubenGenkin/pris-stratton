ed636c5169d0ad54d3cc77568488e780707d2216
Reuben <rleibgenkin@gmail.com>
2021-05-22 21:09:27 -0400

    changes to UI/UX

--- assets/js.js
+++ assets/js.js
@@ -1,17 +1,25 @@
 
-
-
 var temp = $('#temp')
 var humidity = $('#humidity')
 var uvIndex = $('uv-index')
 var list = $('#cities-list')
+var apiKey = `310e39ae7faff36a3a95839e89705b3f`
+var apiUrl = "";
 
 $('#search').click(function () {
     citySearch = $('#input-search').val();
     console.log(citySearch)
-    $('#input-search').val("");
 
-    addCities()
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
+
+    $('#input-search').val("");
+    
+    addCities();
+    fuckFetch();
+});
+
+$('#clear-button').click(function () {
+    $('li').remove()
 });
 
 function addCities() {
@@ -21,4 +29,10 @@ function addCities() {
         $("#cities-list").append(cityItem)
     }
 
+}
+
+function fuckFetch () {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data))
 }
